f92627d373febf5a5b34633ad10ae06487be0175
ttwthomas <ttwthomas@gmail.com>
2021-12-15 12:21:18 -0500

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,10 @@
+instances=$(aws ec2 run-instances --image-id ami-0029863845aafa9de --instance-type t3.micro --key-name canette --security-group-ids sg-0a566a38d0b1ec84c --user-data file://userdata.sh --count 3 --query "Instances[*].InstanceId" --output text)
+echo "waiting for $instances"
+
+for instance in $instances; do
+        aws ec2 wait instance-status-ok --instance-ids $instance
+        sleep 15
+        ip=$(aws ec2 describe-instances --instance-ids $instance --query "Reservations[*].Instances[0].PublicIpAddress" --output text)
+        echo "recording $instance $ip"
+        docker run -d -v $PWD:/out widerin/vnc-recorder --host $ip --port 5901 --password raspberry --crf 25 --outfile /out/$instance.mp4
+done
